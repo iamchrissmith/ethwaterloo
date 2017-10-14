264e51c702831a2adc98ff6fcd5ec86e7f369dfd
Noah Marconi <noah.marconi@gmail.com>
2017-10-14 12:32:02 -0400

    use array literal

--- contracts/Sphere.sol
+++ contracts/Sphere.sol
@@ -36,10 +36,7 @@ contract Sphere {
         memberExists[0xCAf4ad7eddb82994B33D5a60b565E3129af80032] = true;
         memberExists[0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9] = true;
 
-        members[0] = 0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9;
-        members[1] = 0x3b3F565B900cE367e989909C2cE470CD0ce62BA7;
-        members[2] = 0xCAf4ad7eddb82994B33D5a60b565E3129af80032;
-        members[3] = 0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9;
+        members = [0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9, 0x3b3F565B900cE367e989909C2cE470CD0ce62BA7, 0xCAf4ad7eddb82994B33D5a60b565E3129af80032, 0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9];
     }
 
     /*----------- Modifiers -----------*/
