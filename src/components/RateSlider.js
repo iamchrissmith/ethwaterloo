4e2859e79e55b68c17774d183ed80bd964f79c83
Chris Smith <me@chrisryansmith.com>
2017-10-14 18:15:10 -0400

    update slider to prevent multiple submissions

--- src/components/RateSlider.js
+++ src/components/RateSlider.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import { Button, Rating, Card } from 'semantic-ui-react'
 
 export default class RateSlider extends Component {
-  state = { rating: 0 }
+  state = { rating: 0, submitted: false }
 
 	handleChange = e => this.setState({ rating: e.target.value })
 	handleSubmit = async e => {
 		const tx = await this.props.submitRating(this.props.address, this.state.rating)
-		this.setState({rating: 0})
+		this.setState({submitted: true})
+	}
+
+	isDisabled = () => {
+		return this.props.address == this.props.currentUser || this.state.submitted
 	}
 
   render() {
@@ -23,12 +27,12 @@ export default class RateSlider extends Component {
 		        { this.props.address.slice(0,10) }
 		      </Card.Meta>
 					<Card.Description>
-						<input type='range' min={0} max={10} value={rating} onChange={this.handleChange} disabled={this.props.address == this.props.currentUser}/>
+						<input type='range' min={0} max={10} value={rating} onChange={this.handleChange} disabled={this.isDisabled()}/>
 				<br />
 				Rating: {rating}
 						<Rating rating={this.state.rating} maxRating={10} disabled={true} hidden={true}/>
 						<br/>
-						<Button primary onClick={this.handleSubmit} disabled={this.props.address == this.props.currentUser}>Submit Rating</Button>
+						<Button primary onClick={this.handleSubmit} disabled={this.isDisabled()}>Submit Rating</Button>
 		      </Card.Description>
 		    </Card.Content>
 		  </Card>
