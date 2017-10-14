54409f86a896a36540255b26945950a85e03e693
Chris Smith <me@chrisryansmith.com>
2017-10-14 11:59:26 -0400

    pull out contract concerns and pass owned test

---
+++ contracts/Membership.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.2;
+
+contract Membership {
+  /**
+   * Members 
+   * * LogNewMember
+   * * addMember 
+   * * getMemberAtIndex
+   * * isMember
+   */
+  // mapping(address => uint) public memberExists;
+  // address[] public members;
+  // uint public maxMember;
+
+  // event LogNewMember(address sender, address newMember, uint index);
+
+  // modifier fromMember {}
+
+  // function Members() {
+  //   addMember(msg.sender);
+  // }
+
+  // function addMember(address newMember) public fromOwner returns(bool success);
+
+  // function getMemberAtIndex(address member) public returns(uint index);
+
+  // function isMember(address member) public returns(bool isMember);
+
+}
---
+++ contracts/Pausable.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.2;
+
+import './Owned.sol';
+
+contract Pausable is Owned {
+  /** 
+   * Pausible
+   * * LogPausedSet
+   * * setPaused
+   * * isPaused
+   */
+  // bool public paused;
+
+  // modifier whenPaused {}
+  // modifier whenNotPaused {}
+
+  // function Pausible(bool isPaused){
+  //   paused = isPaused;
+  // }
+
+  // function setPaused(bool newState) public fromOwner returns(bool success);
+}
