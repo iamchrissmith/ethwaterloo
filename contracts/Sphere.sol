4530a049a4d1aafb01c16da5fdf0ef5f24494b7e
Noah Marconi <noah.marconi@gmail.com>
2017-10-14 12:12:14 -0400

    formatting

--- contracts/Sphere.sol
+++ contracts/Sphere.sol
@@ -3,55 +3,69 @@ pragma solidity ^0.4.2;
 contract Sphere {
 
 
-  uint256 one;
-  mapping(address => bool) public memberExists;
-  address[] public members;
-
- function Sphere(uint256 _one) {
-   one = _one;
-
-   memberExists[0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9] = true;
-   memberExists[0x3b3F565B900cE367e989909C2cE470CD0ce62BA7] = true;
-   memberExists[0xCAf4ad7eddb82994B33D5a60b565E3129af80032] = true;
-   memberExists[0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9] = true;
-
-   members[0] = 0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9;
-   members[1] = 0x3b3F565B900cE367e989909C2cE470CD0ce62BA7;
-   members[2] = 0xCAf4ad7eddb82994B33D5a60b565E3129af80032;
-   members[3] = 0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9;
- }
-
-  modifier fromMember() {
-    require(memberExists[msg.sender]);
-    _;
-  }
-
-  /**
-   * Rating
-   * * addRatingToMember(address, uint)
-   * * * check if we completed last survey round
-   * * * set new base/total
-   * * * check if we completed this survey, emit LogCompleteRating event
-   * * LogCompleteRating
-   * * getRatingForMember
-   */
+    /*----------- Types -----------*/
 
     struct Rating {
-      uint256 count;
-      uint256 total;
+        uint256 count;
+        uint256 total;
     }
+
+    /*----------- Globals -----------*/
+
+    uint256 one;
+
+    mapping(address => bool) public memberExists;
+
+    address[] public members;
 
     mapping(address => Rating) public ratings;
 
+
+    /*----------- Events -----------*/
+
     event LogCompleteRating(address member, uint256 avgRating);
 
+
+    /*----------- Constructor -----------*/
+
+    function Sphere(uint256 _one) {
+        one = _one;
+
+        memberExists[0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9] = true;
+        memberExists[0x3b3F565B900cE367e989909C2cE470CD0ce62BA7] = true;
+        memberExists[0xCAf4ad7eddb82994B33D5a60b565E3129af80032] = true;
+        memberExists[0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9] = true;
+
+        members[0] = 0xBd79c7E5ae6a8604418832Cf8596b56Fb4D40eC9;
+        members[1] = 0x3b3F565B900cE367e989909C2cE470CD0ce62BA7;
+        members[2] = 0xCAf4ad7eddb82994B33D5a60b565E3129af80032;
+        members[3] = 0x8e6a7eEc280a7CB433d9ef66cC60f88F12eDf8a9;
+    }
+
+    /*----------- Modifiers -----------*/
+
+
+    modifier fromMember() {
+        require(memberExists[msg.sender]);
+        _;
+    }
+
+    /**
+     * Rating
+     * * addRatingToMember(address, uint)
+     * * * check if we completed last survey round
+     * * * set new base/total
+     * * * check if we completed this survey, emit LogCompleteRating event
+     * * LogCompleteRating
+     * * getRatingForMember
+     */
     function addRatingToMember(address member, uint256 rating) public fromMember returns(bool success) {
-      // TODO: restrict 1 rating per period.
+        // TODO: restrict 1 rating per period.
 
-      ratings[member].count += one;
-      ratings[member].total += rating;
+        ratings[member].count += one;
+        ratings[member].total += rating;
 
-      return true;
+        return true;
     }
 
     /*function getRatingForMember(address member) public returns(uint rating);*/
